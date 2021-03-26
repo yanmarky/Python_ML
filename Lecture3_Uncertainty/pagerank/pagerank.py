095d0bffbd2d8f534ef702ed83aa8fa7f13b7226
yanmarky <yan.marky@gmail.com>
2021-03-25 21:58:57 -0400

    Pagerank code

--- Lecture3_Uncertainty/pagerank/pagerank.py
+++ Lecture3_Uncertainty/pagerank/pagerank.py
@@ -2,10 +2,11 @@ import os
 import random
 import re
 import sys
+from numpy.random import choice
 
 DAMPING = 0.85
-SAMPLES = 10000
-
+SAMPLES = 100000
+epsilon = 0.001
 
 def main():
     if len(sys.argv) != 2:
@@ -57,7 +58,29 @@ def transition_model(corpus, page, damping_factor):
     linked to by `page`. With probability `1 - damping_factor`, choose
     a link at random chosen from all pages in the corpus.
     """
-    raise NotImplementedError
+   # initialize dictionay with keys from corpus
+    transition = {}
+    for i in list(corpus.keys()):
+       transition[i] = 0
+
+   # For all the pages the current page has link to, random select one and assign damping_factor to it
+    if len(corpus[page]) > 0:
+        transition[random.sample(corpus[page],1)[0]] = damping_factor
+    else: # giving equal probability to all pages in the corpus
+        for p in transition:
+            transition[p] = 1/len(list(corpus.keys()))
+        return transition
+    
+   # For pages not linked to the given page, randomly pick one and assign 1-damping_factor
+   # transition[random.sample([i for i in list(corpus.keys()) 
+   #                           if i not in corpus[page]],1)[0]] = round(1 - damping_factor,5)
+   
+   # random choose a page 
+    transition[random.sample(list(corpus.keys()),1)[0]] += round(1 - damping_factor,5) 
+    
+    
+    return transition
+    #raise NotImplementedError
 
 
 def sample_pagerank(corpus, damping_factor, n):
@@ -69,7 +92,45 @@ def sample_pagerank(corpus, damping_factor, n):
     their estimated PageRank value (a value between 0 and 1). All
     PageRank values should sum to 1.
     """
-    raise NotImplementedError
+    
+    # Initialize count
+    count = 0
+    
+    # get list of pages
+    pagelist = list(corpus.keys())
+    
+    # Initialize results
+    res = {}
+    for i in pagelist:
+       res[i] = 0
+    
+    # choose the first page
+    page = random.sample(pagelist,1)[0]
+    
+    # sampling   
+    while count <= n:
+        # get transition model 
+        transitionModel = transition_model(corpus, page, damping_factor)
+        
+        # random next page
+        nextPage = choice(list(transitionModel.keys()),
+                          1,
+                          p = list(transitionModel.values()))[0]
+        
+        # update count
+        res[nextPage] += 1
+        
+        # update page and count
+        page = nextPage
+        count += 1
+        
+    # normalize probabilities
+    for x in res:
+        res[x] = res[x]/n
+    
+    return res
+    
+    #raise NotImplementedError
 
 
 def iterate_pagerank(corpus, damping_factor):
@@ -81,7 +142,71 @@ def iterate_pagerank(corpus, damping_factor):
     their estimated PageRank value (a value between 0 and 1). All
     PageRank values should sum to 1.
     """
-    raise NotImplementedError
+
+    # Initialize flag for loop
+    flag = True
+    
+    # get list of pages
+    pagelist = list(corpus.keys())
+    
+    # Initialize results
+    res = {}
+    for i in pagelist:
+       res[i] = 0
+    
+    
+    
+    # Using the iterative algorithm
+    while flag:
+      nextRes = res.copy()
+      for p in nextRes:
+          nextRes[p] = update_pagerank(corpus,p,damping_factor,nextRes)
+          
+      # Check for convergence
+      for p in nextRes:
+          if abs(nextRes[p] - res[p]) >= epsilon:
+              break
+          flag = False
+      
+      # Update results
+      res = nextRes.copy()
+      
+    return res
+    
+    
+    #raise NotImplementedError
+
+def update_pagerank(corpus,page,damping_factor,pageRank):
+    """
+    Return the updated PageRank for the given page by applying the iterative 
+    formula
+
+    """
+    pagelist = list(corpus.keys())
+    
+    d = damping_factor
+    
+    n = len(pagelist)
+    
+    # get incoming pages for the given page
+    incomingPage = []
+    for p in corpus:
+        if page in corpus[p]:
+            incomingPage.append(p)
+    
+    # calcualte the second part of the formula
+    summation = 0
+    for p in incomingPage:
+        numLinks = len(corpus[p])
+        pageRank_p = pageRank[p]
+        summation += pageRank_p/numLinks
+    
+    
+    updatedPageRank= (1-d)/n + d*summation
+    
+    return updatedPageRank
+     
+     
 
 
 if __name__ == "__main__":
